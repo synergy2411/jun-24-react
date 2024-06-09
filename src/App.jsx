99b6bf1affc0b6b89daf195ea11ba88ee38362c5
Sumit Khandelwal <synergy2411@outlook.com>
2024-06-09 15:24:04 +0530

    uncontrolled element added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import ClassBased from "./Components/Playground/ClassBased";
+import UserLogin from "./Components/Playground/UserLogin";
 
 function App() {
-  const [show, setShow] = useState(true);
+  // const [show, setShow] = useState(true);
 
   return (
     <div>
       <h1>App Works!</h1>
-      <button onClick={() => setShow(!show)} className="btn btn-secondary">
+
+      <UserLogin />
+
+      {/* <button onClick={() => setShow(!show)} className="btn btn-secondary">
         {show ? "Hide" : "Show"}
-      </button>
+      </button> */}
       {/* <Expenses /> */}
       {/* {show && <ClassBased />} */}
 
-      <ClassBased value="Component A" />
+      {/* <ClassBased value="Component A" /> */}
 
       {/* <hr />
       <ClassBased value="Component B" /> */}
